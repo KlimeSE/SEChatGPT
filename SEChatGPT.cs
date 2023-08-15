5c60f71206f6b248a078ba8cd5ad237c5a7b87cb
KlimeSE <68099172+KlimeSE@users.noreply.github.com>
2023-08-15 23:20:29 +0100

    Basic

--- SEChatGPT.cs
+++ SEChatGPT.cs
@@ -1,7 +1,22 @@
-﻿namespace SEChatGPT
-{
-    public class SEChatGPT
-    {
+﻿using VRage.Plugins;
 
+namespace SEChatGPT
+{
+    public class SEChatGPT : IPlugin
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(object gameInstance)
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
     }
 }
